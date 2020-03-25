d4916f28844355c7d824133d0b7bdc9dd3b7c03d
Pheeranut First <pheeranut.firstject@mail.kmutt.ac.th>
2020-03-25 19:21:48 +0700

    Add Inspector Tab: Level Settings

--- Project/src/scenes/editor/InspectorPanel/LevelTab/LevelTab.gd
+++ Project/src/scenes/editor/InspectorPanel/LevelTab/LevelTab.gd
@@ -1,7 +1,7 @@
-# Inspector
+# LevelTab
 # Written by: First
 
-extends Control
+extends Tabs
 
 #class_name optional
 
@@ -21,27 +21,13 @@ extends Control
 #      Constants
 #-------------------------------------------------
 
-const INSPECTOR_MIN_LEFT_MARGIN = -160
-const INSPECTOR_MAX_LEFT_MARGIN = 200 #Margin at which the inspector will automatically hide
-const DEFAULT_INSPECTOR_LEFT_MARGIN = 0
-
 #-------------------------------------------------
 #      Properties
 #-------------------------------------------------
 
-onready var panel_open = $PanelOpen
-onready var panel_close = $PanelClose
-onready var inspector_show_btn = $PanelClose/InspectorShowBtn
-onready var inspector_hide_btn = $PanelOpen/ContentVBox/InspectorHideBtn
-
-var resize_dragging = false
-
 #-------------------------------------------------
 #      Notifications
 #-------------------------------------------------
-
-func _ready() -> void:
-	hide_inspector()
 
 #-------------------------------------------------
 #      Virtual Methods
@@ -55,40 +41,9 @@ func _ready() -> void:
 #      Public Methods
 #-------------------------------------------------
 
-func show_inspector():
-	panel_open.visible = true
-	panel_close.visible = false
-
-func hide_inspector():
-	panel_open.visible = false
-	panel_close.visible = true
-
-func resize_drag_ended():
-	if panel_open.margin_left > INSPECTOR_MAX_LEFT_MARGIN:
-		panel_open.margin_left = DEFAULT_INSPECTOR_LEFT_MARGIN
-		hide_inspector()
-	if panel_open.margin_left < INSPECTOR_MIN_LEFT_MARGIN:
-		panel_open.margin_left = INSPECTOR_MIN_LEFT_MARGIN
-
 #-------------------------------------------------
 #      Connections
 #-------------------------------------------------
-
-func _on_InspectorHideBtn_pressed() -> void:
-	hide_inspector()
-
-func _on_InspectorShowBtn_pressed() -> void:
-	show_inspector()
-
-func _on_ResizeHandler_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		resize_dragging = event.is_pressed()
-		if not resize_dragging:
-			resize_drag_ended()
-	
-	if resize_dragging:
-		if event is InputEventMouseMotion:
-			panel_open.margin_left += event.relative.x
 
 #-------------------------------------------------
 #      Private Methods
@@ -97,4 +52,3 @@ func _on_ResizeHandler_gui_input(event: InputEvent) -> void:
 #-------------------------------------------------
 #      Setters & Getters
 #-------------------------------------------------
-
