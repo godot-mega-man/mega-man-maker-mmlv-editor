a4baf94488cd3167caf31cfd454d9be7a9e1580d
Pheeranut First <pheeranut.firstject@mail.kmutt.ac.th>
2020-04-11 19:01:10 +0700

    Change how 0.x warning message appears by showing it only once

--- Project/src/scenes/editor/Popups/VersionZeroWarningDialog.gd
+++ Project/src/scenes/editor/Popups/VersionZeroWarningDialog.gd
@@ -22,6 +22,8 @@ extends AcceptDialog
 #      Constants
 #-------------------------------------------------
 
+const FIRST_TIME_FILE_NAME = "ZeroVersionOpened.txt"
+
 #-------------------------------------------------
 #      Properties
 #-------------------------------------------------
@@ -34,7 +36,8 @@ func _ready() -> void:
 	if not is_version_zero():
 		queue_free()
 		return
-	if self.is_visible():
+	if not is_first_time_open():
+		queue_free()
 		return
 	
 	#Popup a warning
@@ -53,14 +56,42 @@ func _ready() -> void:
 #-------------------------------------------------
 
 func is_version_zero() -> bool:
-	return (ProjectSettings.get_setting("application/config/version") as String).begins_with("0")
+	return get_version().begins_with("0")
 
-func _on_VersionZeroWarningDialog_popup_hide() -> void:
-	queue_free()
+func is_first_time_open() -> bool:
+	var f = File.new()
+	var file_path = "user://" + FIRST_TIME_FILE_NAME
+	var is_file_exist = f.file_exists(file_path)
+	var ver_txt : String
+	
+	if is_file_exist:
+		f.open(file_path, File.READ)
+		ver_txt = f.get_as_text()
+		f.close()
+	
+	if not is_file_exist:
+		return true
+	
+	return ver_txt != get_version()
+
+func save_first_time_opened():
+	var f = File.new()
+	var file_path = "user://" + FIRST_TIME_FILE_NAME
+	
+	f.open(file_path, File.WRITE)
+	f.store_string(get_version())
+	f.close()
+
+func get_version() -> String:
+	return ProjectSettings.get_setting("application/config/version")
 
 #-------------------------------------------------
 #      Connections
 #-------------------------------------------------
+
+func _on_VersionZeroWarningDialog_popup_hide() -> void:
+	save_first_time_opened()
+	queue_free()
 
 #-------------------------------------------------
 #      Private Methods
