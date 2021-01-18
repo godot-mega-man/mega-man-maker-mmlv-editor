ecc6eaf06d411aecf07706b46690db5c9c002bb4
Snowflakes <plotrunn343@gmail.com>
2021-01-19 00:34:07 +0700

    Fix when Tile Grid is enabled, the camera speed seems to be slowed down

--- Project/src/scenes/editor/ViewportEventKeyScroller/ViewportEventKeyScroller.gd
+++ Project/src/scenes/editor/ViewportEventKeyScroller/ViewportEventKeyScroller.gd
@@ -27,9 +27,10 @@ signal moving(velocity)
 #      Properties
 #-------------------------------------------------
 
-export (float) var default_scroll_speed = 20
+export (float) var default_scroll_speed = 1800
 
 var speed_modifier : float
+var speed_process : float
 
 #-------------------------------------------------
 #      Notifications
@@ -62,16 +63,17 @@ func _input_process():
 	if Input.is_key_pressed(KEY_CONTROL):
 		return
 	
-	speed_modifier = default_scroll_speed * (1 + int(Input.is_key_pressed(KEY_SHIFT)))
+	speed_modifier = (1 + int(Input.is_key_pressed(KEY_SHIFT)))
+	speed_process = default_scroll_speed * get_process_delta_time()
 	
 	if Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A):
-		emit_signal("moving", Vector2.LEFT * speed_modifier)
+		emit_signal("moving", Vector2.LEFT * speed_process * speed_modifier)
 	if Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
-		emit_signal("moving", Vector2.RIGHT * speed_modifier)
+		emit_signal("moving", Vector2.RIGHT * speed_process * speed_modifier)
 	if Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W):
-		emit_signal("moving", Vector2.UP * speed_modifier)
+		emit_signal("moving", Vector2.UP * speed_process * speed_modifier)
 	if Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S):
-		emit_signal("moving", Vector2.DOWN * speed_modifier)
+		emit_signal("moving", Vector2.DOWN * speed_process * speed_modifier)
 
 #-------------------------------------------------
 #      Setters & Getters
