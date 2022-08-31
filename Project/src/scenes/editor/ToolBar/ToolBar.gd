4869855b0ca50ebadea043e046b1be67d2efcc90
DESKTOP-TPL2BMK\Daniel <daniel.r.durda@gmail.com>
2022-08-31 16:25:36 -0400

    added a bunch of spikes and ladders. Changed all the icons to be orange instead of green. Added a lock screen button

--- Project/src/scenes/editor/ToolBar/ToolBar.gd
+++ Project/src/scenes/editor/ToolBar/ToolBar.gd
@@ -20,6 +20,9 @@ extends Panel
 signal add_object_pressed
 
 signal pressed
+signal lock_screen
+
+onready var lock_btn = $HBoxContainer/EditModeVBox2/HBoxContainer/LockBtn
 
 #-------------------------------------------------
 #      Constants
@@ -49,6 +52,10 @@ signal pressed
 #      Connections
 #-------------------------------------------------
 
+func _ready() -> void:
+	var x = get_node("HBoxContainer/EditModeVBox2/HBoxContainer/LockBtn")
+	x.connect("lock_screen", self, "_on_lock_screen")
+
 func _on_AddBtn_pressed() -> void:
 	emit_signal("add_object_pressed")
 
@@ -76,10 +83,16 @@ func _on_LadderBtn_pressed() -> void:
 func _on_SpikeBtn_pressed() -> void:
 	EditMode.set_mode(EditMode.Mode.SPIKE)
 	emit_signal("pressed")
+	
+func _on_LockBtn_pressed() -> void:
+	EditorConfig.locked_keyboard = !EditorConfig.locked_keyboard
 
 func _on_ButtonsToggler_pressed() -> void:
 	SelectedObjects.remove_all()
 
+func _on_lock_screen() -> void:
+	print_debug("banana")
+	lock_btn.set_pressed(EditorConfig.locked_keyboard) 
 #-------------------------------------------------
 #      Private Methods
 #-------------------------------------------------
