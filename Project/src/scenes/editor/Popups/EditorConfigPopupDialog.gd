fdfaae53d8300990b82c7afaf040daace6a8b979
DESKTOP-TPL2BMK\Daniel <daniel.r.durda@gmail.com>
2022-12-20 12:13:38 -0500

    merge

--- Project/src/scenes/editor/Popups/EditorConfigPopupDialog.gd
+++ Project/src/scenes/editor/Popups/EditorConfigPopupDialog.gd
@@ -20,13 +20,14 @@ extends WindowDialog
 #-------------------------------------------------
 #      Properties
 #-------------------------------------------------
-
+onready var advanced_area = $MarginContainer/VBox/ScrollContainer/VBox/AdvancedMode
 onready var auto_update_checkbox := $MarginContainer/VBox/ScrollContainer/VBox/AutoUpdate/CheckBox
 onready var reduced_motion_checkbox := $MarginContainer/VBox/ScrollContainer/VBox/ReducedMotion/CheckBox
 onready var locked_keyboard := $MarginContainer/VBox/ScrollContainer/VBox/LockedKeyboard/CheckBox
 onready var camera_smoothness_lineedit := $MarginContainer/VBox/ScrollContainer/VBox/CameraSmoothness/LineEdit
 onready var max_recent_files_lineedit := $MarginContainer/VBox/ScrollContainer/VBox/MaxRecentFiles/LineEdit
 onready var fps_lineedit := $MarginContainer/VBox/ScrollContainer/VBox/Fps/LineEdit
+onready var advanced_mode : = $MarginContainer/VBox/ScrollContainer/VBox/AdvancedMode/CheckBox
 
 
 #-------------------------------------------------
@@ -49,6 +50,7 @@ func apply_changes():
 	EditorConfig.auto_check_update = auto_update_checkbox.pressed
 	EditorConfig.reduced_motion = reduced_motion_checkbox.pressed
 	EditorConfig.locked_keyboard = locked_keyboard.pressed
+	EditorConfig.advanced_mode =  advanced_mode.pressed
 	EditorConfig.camera_smoothness = int(camera_smoothness_lineedit.text)
 	EditorConfig.max_recent_files = int(max_recent_files_lineedit.text)
 	EditorConfig.fps = int(fps_lineedit.text)
@@ -63,10 +65,15 @@ func _on_EditorConfigPopupDialog_about_to_show() -> void:
 
 func _on_OkButton_pressed() -> void:
 	apply_changes()
+	advanced_area.visible = false
 	hide()
 
 func _on_CancelButton_pressed() -> void:
+	advanced_area.visible = false
 	hide()
+
+func _on_HiddenButton_pressed() -> void:
+	advanced_area.visible = !advanced_area.visible
 
 func _on_CameraSmoothness_LineEdit_unfocused() -> void:
 	camera_smoothness_lineedit.text = str(
@@ -103,6 +110,7 @@ func _update_ui_from_config_autoload():
 	auto_update_checkbox.pressed = EditorConfig.auto_check_update
 	reduced_motion_checkbox.pressed = EditorConfig.reduced_motion
 	locked_keyboard.pressed = EditorConfig.locked_keyboard
+	advanced_mode.pressed = EditorConfig.advanced_mode
 	camera_smoothness_lineedit.text = str(EditorConfig.camera_smoothness)
 	max_recent_files_lineedit.text = str(EditorConfig.max_recent_files)
 	fps_lineedit.text = str(EditorConfig.fps)
